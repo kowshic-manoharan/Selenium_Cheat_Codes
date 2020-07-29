e60c367853803b35842f4215306627e21b83f016
kowshic <smartkowshic@gmail.com>
2020-07-29 20:19:11 +0530

    TestNG Base Code

---
+++ TestNG/src/test/java/testng/WelcomeTestNG.java
@@ -0,0 +1,19 @@
+/**
+ * This is the basic introduction of the TestNg 
+ */
+package testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author kowshic
+ * @version Selenium 3.0
+ */
+public class WelcomeTestNG {
+	
+	@Test
+	public void demo() {
+		System.out.println("Successfully entered into TestNG");
+	}
+
+}
